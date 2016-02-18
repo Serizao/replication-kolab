fcac410bcee46711a3b27076a6c50601b2746e33
serizao <william.leberre@gmail.com>
2016-02-18 11:20:02 +0100

    v2

--- script.sh
+++ script.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 
-
-hostmaster="192.168.2.1"
+hostmaster="51.254.122.248"
 usermaster="root"
-hostslave="192.168.2.2"
+hostslave="51.254.122.251"
+
 userslave="root"
 
 
+rm -rf /tmp/$nsdbmaster.ldif 
+ssh $userslave@$hostslave "rm -rf /tmp/$nsdbmaster.ldif"
+ssh $userslave@$hostslave 'service dirsrv stop'
 
-####extraction ldap#####
+###extraction ldap#####
 dirmaster=$(find /etc/dirsrv/ -mindepth 1 -maxdepth 1 -type d -name "slapd-*" | xargs -n 1 basename)
 nsdbmaster=$(find /var/lib/dirsrv/$dirmaster/db/ -mindepth 1 -maxdepth 1 -type d | xargs -n 1 basename)
 ns-slapd db2ldif -D /etc/dirsrv/$dirmaster -n $nsdbmaster -a /tmp/$nsdbmaster.ldif >/dev/null 2>&1
 
 
-##definition des variable du slave
+###definition des variable du slave####
 dirslave=$( ssh $userslave@$hostslave 'find /etc/dirsrv/ -mindepth 1 -maxdepth 1 -type d -name "slapd-*" | xargs -n 1 basename')
 cmd="find /var/lib/dirsrv/$dirslave/db/ -mindepth 1 -maxdepth 1 -type d | xargs -n 1 basename"
 nsdbslave=$(ssh $userslave@$hostslave $cmd)
 
 
-#deverseversement de l'extraction####
-scp /tmp/$nsdbmaster.ldif $userslave@$hostslave:/tmp
-ssh $userslave@$hostslave "chmod 777 /tmp/$nsdbmaster.ldif"
-ssh $userslave@$hostslave 'service dirsrv stop'
-ssh $userslave@$hostslave "ns-slapd ldif2db -D /etc/dirsrv/$dirslave  -n $nsdbslave  -i /tmp/$nsdbmaster.ldif"
-ssh $userslave@$hostslave 'service dirsrv start'
+echo "################################"
+echo "debut du cron le $(date +%y-%m-%d-%H:%M:%S)"
+
+###deverseversement de l'extraction####
+/usr/lib/dirsrv/$dirmaster/db2bak /tmp/$nsdbmaster.ldif
+scp -r /tmp/$nsdbmaster.ldif $userslave@$hostslave:/tmp >> /var/log/replica/journal.log
+ssh $userslave@$hostslave "chmod -R 777 /tmp/$nsdbmaster.ldif"
+ >> /var/log/replica/journal.log
+ssh $userslave@$hostslave "/usr/lib/dirsrv/$dirmaster/bak2db /tmp/$nsdbmaster.ldif/"
+ssh $userslave@$hostslave "service dirsrv start"
 
 ### synchronisation des boites mail#####
-unison /var/lib/imap/ ssh://web.opensrc.fr//var/lib/imap/
-unison /var/spool/imap/ ssh://web.opensrc.fr//var/spool/imap/
+unison /var/lib/imap/ ssh://web.opensrc.fr//var/lib/imap/ >> /var/log/replica/journal.log
+unison /var/spool/imap/ ssh://web.opensrc.fr//var/spool/imap/ >> /var/log/replica/journal.log
 ssh $userslave@$hostslave 'chown -R cyrus:mail /var/spool/imap'
+echo "fin du cron le $(date +%y-%m-%d-%H:%M:%S)"
+
+
